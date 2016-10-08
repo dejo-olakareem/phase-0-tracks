2455c09b275c445456e496e9bab83f0934c80419
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-08 13:22:59 -0700

    made final programming to method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,33 +1,47 @@
-string="abc"
-def encrypt(string)
-	i=0
-	while i < 1
-	string1= string[0].next
-	string2= string[1].next
-	string3= string[2].next
-	p string1 + string2 + string3
-	i += 1
+# 
+
+
+
+
+
+p "Would you like to encrypt or decrypt a password?"
+respond = gets.chomp
+
+
+
+p "What is the password?"
+password = gets.chomp
+
+
+if respond  == "encrypt"
+	def encrypt(string)
+		i=0
+		while i < string.length
+		string1= string[i].next
+		i +=1
+	
+			if string1 == "aa"
+				string1 = "a"
+			end
+			p string1
+		end
 	end
-end
-p encrypt("abc")
+	p encrypt(password)
+ 	
 
-
-
-
-def decrypt(string_to_be_decrypted)
-	string = "abcdefghijklmnopqrstuvwxyz"
-	string1 = string_to_be_decrypted[0] 
-	equation = string.index(string1)-1
-	string[equation]
+elsif respond == "decrypt"
 	
-	 string2 = string_to_be_decrypted[1]
-	equation2 = string.index(string2)-1
-	string[equation2]
+	def decrypt(string_to_be_decrypted)
+		i= 0
+		while i < string_to_be_decrypted.length
+		string = "abcdefhgijklmnopqrstuvwxyz"
+		string1 = string_to_be_decrypted[i]
+		equation_index = string.index(string1)-1
+		p answer= string[equation_index]
+		i +=1
+		end
+	end
+	p decrypt(password)
+end	
 	
-	string3= string_to_be_decrypted[2]
-	equation3 = string.index(string3)-1
-	string[equation3]
-	
-	puts string4 = string[equation] +string[equation2]+ string[equation3]
-end
-puts decrypt("afe")
+ decrypt(encrypt("swordfish"))
