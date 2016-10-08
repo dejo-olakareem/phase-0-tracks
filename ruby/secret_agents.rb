9265ee2ccf37bd338129d914b6004ea4b99ead0d
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-08 14:47:31 -0700

    add changes to method to add stings together

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -16,15 +16,20 @@ password = gets.chomp
 if respond  == "encrypt"
 	def encrypt(string)
 		i=0
+		encrypted_word = ""
 		while i < string.length
 		string1= string[i].next
 		i +=1
 	
 			if string1 == "aa"
 				string1 = "a"
+				elsif string1 == "!"
+				string1 = " "
+				
 			end
-			p string1
+			encrypted_word += string1
 		end
+		return encrypted_word
 	end
 	p encrypt(password)
  	
@@ -33,15 +38,17 @@ elsif respond == "decrypt"
 	
 	def decrypt(string_to_be_decrypted)
 		i= 0
+		decrypted_word=""
 		while i < string_to_be_decrypted.length
 		string = "abcdefhgijklmnopqrstuvwxyz"
 		string1 = string_to_be_decrypted[i]
 		equation_index = string.index(string1)-1
-		p answer= string[equation_index]
+		decrypted_word += string[equation_index]
 		i +=1
 		end
+		return decrypted_word
 	end
 	p decrypt(password)
 end	
 	
- decrypt(encrypt("swordfish"))
+ #decrypt(encrypt("swordfish"))
