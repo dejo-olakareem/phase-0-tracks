abb055f0d6d344ff638fb196e0711842d1cd392d
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-08 15:12:20 -0700

    add pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,17 +1,23 @@
-# 
-
-
+# Ask user for function
 
 
 
 p "Would you like to encrypt or decrypt a password?"
 respond = gets.chomp
 
-
+# Ask user for password
 
 p "What is the password?"
 password = gets.chomp
 
+
+# Activate users response for encrypt
+# let's use .length to loop 
+# through the word
+# Enter condition so space character remains a space character
+# Enter condition statement for edge case
+# Gather data with encrypted_word
+# Return string
 
 if respond  == "encrypt"
 	def encrypt(string)
@@ -33,6 +39,15 @@ if respond  == "encrypt"
 	end
 	p encrypt(password)
  	
+# Activate users response for decrypt
+# let's use .length to loop 
+# through the word
+# Gather data with encrypted_word
+# Return string
+
+
+
+
 
 elsif respond == "decrypt"
 	
@@ -40,11 +55,11 @@ elsif respond == "decrypt"
 		i= 0
 		decrypted_word=""
 		while i < string_to_be_decrypted.length
-		string = "abcdefhgijklmnopqrstuvwxyz"
-		string1 = string_to_be_decrypted[i]
-		equation_index = string.index(string1)-1
-		decrypted_word += string[equation_index]
-		i +=1
+			string = "abcdefhgijklmnopqrstuvwxyz"
+			string1 = string_to_be_decrypted[i]
+			equation_index = string.index(string1)-1
+			decrypted_word += string[equation_index]
+			i +=1
 		end
 		return decrypted_word
 	end
