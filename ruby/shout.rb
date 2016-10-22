62117efa299c2fe98848a78b98e8d9aab57bf620
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-22 11:23:21 -0700

    add drive code to shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,29 +1,22 @@
-# module Shout
-#  def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#  end
-
-#  def self.yell_happily(words)
-#  	words + "!!!" + ":)"
-#  end
-
-# end
-
-
-
-
-
-# Shout.yell_angrily("screwyou")
-# Shout.yell_happily("Nice to meet you")
-
 module Shout
+	def brag(words)
+	  puts "#{words} is really making me upset !!!!"
+	end
 end
 
 
 
 class Destroy
+	include Shout
 end
 
 
 class Anger
+	include Shout
 end
+
+
+beat = Destroy.new
+beat.brag("wale")
+fire = Anger.new
+fire.brag("taryn")
