19542a2b33d517dbe6c8094d1c2405896a1bdd22
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-25 20:58:59 -0700

    add function car to the script

--- js/data_structures.js
+++ js/data_structures.js
@@ -16,4 +16,29 @@ for(var i = 0;i < colors.length;i++){
 }
 console.log(horse);
 
+function Car (maxSpeed,color,isAutomatic){
+  
+  this.maxSpeed = maxSpeed;
+  this.color = color;
+  this.isAutomatic = isAutomatic;
+
+  this.brightLight = function(a) {
+    console.log("shine bright like a diamond:" + a);
+  };
+  
+  
+}
+
+var car1 = new Car(80, "red", false);
+console.log(car1);
+car1.brightLight(8);
+
+var car2 = new Car(35, "rust", true);
+console.log(car2);
+car2.brightLight(0);
+
+
+var car3 = new Car(150, "white", false);
+console.log(car3);
+car3.brightLight(10);
 
