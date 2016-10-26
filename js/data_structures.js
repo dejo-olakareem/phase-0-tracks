7124a90418460efc588b61bd0d48eaee7a049579
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-25 19:48:16 -0700

    add colors to color arr and names

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["blue","red","green","black"];
+var names = ["bob","jack","jane","romaro"];
+
+
+
+colors.push("white");
+names.push("ricardo");
+
+console.log(colors);
+console.log(names);
