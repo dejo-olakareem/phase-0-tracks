4b45eb5a5dbb40af8cf33998f16f31dd6858f712
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-27 08:49:30 -0700

    add pseudocode and wrote code for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,22 @@
+
+// Create a function that takes in a string as an input
+// Iterate over the input string from the last word
+// Create an empty variable 
+// Add each index to the empty variable
+// Return the Variable 
+
+
+
+
+
+function reverse(str){
+	fav = "";
+	for(i = str.length-1; i>=0;i--){
+		fav += str[i];
+		
+	}
+	return (fav);
+}
+
+reverse("hello")
+
