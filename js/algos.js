870dae7d804c06eb5052216de9bf4cbea4da241e
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-10-27 16:05:59 -0700

    add object comparisom function and drive code

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,7 @@
 // Create a function
 // Function should take in Array as input
 // Iterate through each Array index
+// we want to store the length of the first index
 // if index is the longest, print index
 
 
@@ -15,3 +16,34 @@ function take_in_array(arr){
 	}
 	return stored
 }
+wale = ["long phrase","longest phrase","longer phrase"]
+console.log(take_in_array(wale))
+
+
+// Create a function that takes in two input
+// iterate over each key
+// check to see if this inputs shares a key
+// print the key that is shared
+obj1 = {name: 66,age: 30,bath : 506};
+
+obj2 = {name: 66,age: 30,bath : 506};
+
+
+ function object_comparism(obj1,obj2){
+ 	for (var i in obj2){
+ 	if (obj1[i] == obj2[i]){
+ 		 v = true;
+ 	}else 
+ 		v = false;
+ 	
+ }	
+ return v 
+}
+
+
+blake = {name: "Steven", age: 54}
+drakula = {name: "Tamir", age: 54}
+
+// object_comparism(blake,drakula)
+object_comparism(obj1,obj2)
+ 
