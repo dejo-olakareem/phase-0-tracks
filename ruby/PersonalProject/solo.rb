566ce87b76e578a6c891a450a8cbe8ed46670639
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-04 14:44:50 -0700

    add execute method to create table in database

--- ruby/PersonalProject/solo.rb
+++ ruby/PersonalProject/solo.rb
@@ -4,7 +4,7 @@ require "sqlite3"
 
 
 # Create A Data base that will collect user information
-SQLite3::Database.new ("retail.db")
+db = SQLite3::Database.new ("retail.db")
 
 # Create a table to store user information into Database
 create_table_cmd = <<-SQL 
@@ -12,10 +12,11 @@ CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255),
-    user_name VARCHAR(255),
+    user_name VARCHAR(255)
     )
 SQL
 
+db.execute(create_table_cmd)
 # Ask for user Information
 
 
