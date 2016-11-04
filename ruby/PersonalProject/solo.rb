9cc0bbfef50593f395b742166c0dc603f5ba7e4b
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-04 14:07:48 -0700

    add sqlite3 to rb page as gem to use'

---
+++ ruby/PersonalProject/solo.rb
@@ -0,0 +1,18 @@
+# RETAIL USER DATABASE
+require "sqlite3"
+
+
+
+# Create A Data base that will collect user information
+
+
+
+# Ask for user Information
+
+
+
+# Insert user Information Into Database
+
+
+
+
