3c0b34da3bfacf0679e7be7977988c54e3c1caf1
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-04 14:21:24 -0700

    add command to create database and table to store user information

--- ruby/PersonalProject/solo.rb
+++ ruby/PersonalProject/solo.rb
@@ -6,7 +6,15 @@ require "sqlite3"
 # Create A Data base that will collect user information
 SQLite3::Database.new ("retail.db")
 
-
+# Create a table to store user information into Database
+create_table_cmd = <<-SQL 
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    user_name VARCHAR(255),
+    )
+SQL
 
 # Ask for user Information
 
