512b60d61d5118c16e0c414af8b7ad3a82528c7c
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-04 16:31:15 -0700

    tried out method to build on

--- ruby/PersonalProject/solo.rb
+++ ruby/PersonalProject/solo.rb
@@ -5,6 +5,7 @@ require "sqlite3"
 
 # Create A Data base that will collect user information
 db = SQLite3::Database.new ("retail.db")
+db.results_as_hash = true
 
 # Create a table to store user information into Database
 create_table_cmd = <<-SQL 
@@ -18,27 +19,39 @@ SQL
 
 db.execute(create_table_cmd)
 # Ask for user Information
-puts "Welcome to Wally's Super store"
-puts "We will like to gather your Info for Purchase"
-puts "What is your full name?,First name Last name"
+# puts "Welcome to Wally's Super store"
+# puts "We will like to gather your Info for Purchase"
+# puts "What is your full name?,First name Last name"
 
-name1 = gets.chomp.split(" ")
+# name1 = gets.chomp.split(" ")
 
-name1.map! do|i|
- i.capitalize
-end
+# name1.map! do|i|
+#  i.capitalize
+# end
 
-p name = name1.join(" ")
+# p name = name1.join(" ")
 
-puts "What is your email"
+# puts "What is your email"
 
-p email = gets.chomp
+# p email = gets.chomp
 
-puts "choice of user name"
-p user_name = gets.chomp
+# puts "choice of user name"
+# p user_name = gets.chomp
 
+# name = "wale"
+# email ="wale@123.com"
+# user_name = "foeverme1"
 
+ db.execute("INSERT INTO users (name, email,user_name) VALUES ('wale','wale@123.com','forever1')")
 
+ p db.execute("SELECT * FROM users")
+# def create_kitten(db,name, email,user_name)
+#   db.execute("INSERT INTO users (name, age,user_name) VALUES (?, ?, ?)", [name, email,user_name])
+# end
+
+# create_kitten(users,"wale","wale@123.com","foeverme1")
+
+# p db.execute("SELECT * FROM users")
 # Insert user Information Into Database
 
 
