9acf4b46197747a42b6934fc1e1b8e7a08fb23e1
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-05 15:17:00 -0700

    finanlize method to update and print data from users tab;e

--- ruby/PersonalProject/solo.rb
+++ ruby/PersonalProject/solo.rb
@@ -32,12 +32,28 @@ def all_info_dab(db)
 	 		end
 end
 
-def specific_user(db,name)
+def specific_user(db,userid)
 	username = db.execute("SELECT users.name FROM users").first
-	db.execute("SELECT * from users where name = '#{name}' ") do |row|
-		puts "This is your  name #{row['name']} and your email is #{row['email']} the username you generated was #{row['user_name']}."
+	db.execute("SELECT * from users where id = #{userid} ") do |row|
+		puts "This is your  name #{row['name']} and your email is #{row['email']} the username you generated was #{row['user_name']}.
+		Keeps this id number #{row['id']}. You can use it to update you information in the future."
 	end
 end
+
+def updatename(db,updatedname,idprovided)
+	db.execute("UPDATE users SET name = '#{updatedname}' where id= '#{idprovided}' ")
+end
+
+def updateemail(db,updatedemail,idprovided)
+	db.execute("UPDATE users SET email = '#{updatedemail}' where id= '#{idprovided}' ")
+end
+
+def updateusername(db,updateduser_name,idprovided)
+	db.execute("UPDATE users SET user_name = '#{updateduser_name}' where id= '#{idprovided}' ")
+end
+
+
+
 
 # Ask for user Information
 # USER INTERFACE
@@ -64,16 +80,42 @@ user_name = gets.chomp
 
 
 create_user(db,name,email,user_name)
-specific_user(db,name)
-puts " Is this correct Y/N"
-spec_ans = gets.chomp.upcase
-if spec_ans =="Y"
-	p "Thanks for your registration"
-	elsif spec_ans == "N"
- 	 p "Which will you like to change? name,email or username"
-else puts "I do not understand you!!!"
-end
+userid = (db.execute("SELECT * FROM users Where name = '#{name}' ")).first['id']
+puts userid
+valid_input = false
+until valid_input == true 
+	specific_user(db,userid)
+	puts " Is this correct Y/N"
+	spec_ans = gets.chomp.upcase
+	if spec_ans =="Y"
+		valid_input = true
+		p "Thanks for your registration"
+		elsif spec_ans == "N"
+			puts "what is your id?"
+				idprovided = gets.chomp.to_i
+	 	 	p "Which will you like to change? name,email or username"
+	 	 	veri_ans = gets.chomp
+		 	if veri_ans == "name"
+		 	 		puts "What will you like to update it to"
+		 	 		name1 = gets.chomp.split(" ")
+						name1.map! do|i|
+						 i.capitalize
+						end
+					updatedname = name1.join(" ")
+					updatename(db,updatedname,idprovided)
+		 	 	elsif veri_ans == "email" 
+		 	 		puts "What email will you like to update to"
+		 	 		updatedemail = gets.chomp
+		 	 		updateemail(db,updatedemail,idprovided)
+		 	 	elsif veri_ans == "username"
+		 	 		puts "What will be your user name"
+		 	 		updateuser_name = gets.chomp
+		 	 		updateusername(db,updateuser_name,idprovided)
+		 	end
 
+		else puts "I do not understand you!!!"
+	end
+end
 
 
 
