a2b2017a1aa35b0255a69e971bcfecb8bb1a6326
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-11-05 18:05:17 -0700

    add final methods newmember and loops to ask for membership

--- ruby/PersonalProject/solo.rb
+++ ruby/PersonalProject/solo.rb
@@ -35,8 +35,8 @@ end
 def specific_user(db,userid)
 	username = db.execute("SELECT users.name FROM users").first
 	db.execute("SELECT * from users where id = #{userid} ") do |row|
-		puts "This is your  name #{row['name']} and your email is #{row['email']} the username you generated was #{row['user_name']}.
-		Keeps this id number #{row['id']}. You can use it to update you information in the future."
+		puts "Your name is #{row['name']} and your email is #{row['email']} the username you generated was #{row['user_name']}.
+		Keep your id number,which is #{row['id']}. You can use it to update you information in the future."
 	end
 end
 
@@ -53,73 +53,90 @@ def updateusername(db,updateduser_name,idprovided)
 end
 
 
+def new_member(db,userid)
+	valid_input = false
+	until valid_input == true 
+	specific_user(db,userid)
+		puts " Is this correct Y/N"
+		spec_ans = gets.chomp.upcase
+		if spec_ans =="Y"
+			valid_input = true
+			p "Thanks for your registration"
+			elsif spec_ans == "N"
+				puts "what is your id?"
+					idprovided = gets.chomp.to_i
+		 	 	p "Which will you like to change? name,email or username"
+		 	 	veri_ans = gets.chomp
+			 	if veri_ans == "name"
+			 	 		puts "What will you like to update it to"
+			 	 		name1 = gets.chomp.split(" ")
+							name1.map! do|i|
+							 i.capitalize
+							end
+						updatedname = name1.join(" ")
+						updatename(db,updatedname,idprovided)
+			 	 	elsif veri_ans == "email" 
+			 	 		puts "What email will you like to update to"
+			 	 		updatedemail = gets.chomp
+			 	 		updateemail(db,updatedemail,idprovided)
+			 	 	elsif veri_ans == "username"
+			 	 		puts "What will be your user name"
+			 	 		updateuser_name = gets.chomp
+			 	 		updateusername(db,updateuser_name,idprovided)
+			 	end
 
+			else puts "I do not understand you!!!"
+		end
+	end
+end
 
 # Ask for user Information
 # USER INTERFACE
 puts "Welcome to Wally's Super store"
-puts "We will like to gather your Info for Purchase"
-puts "What is your full name?,First name Last name"
 
-name1 = gets.chomp.split(" ")
+valid_input =false
+until valid_input == true
 
-name1.map! do|i|
- i.capitalize
-end
+	puts "Are you a new member or current member?"
+	member = gets.chomp
 
-name = name1.join(" ")
-
-puts "What is your email"
-
-email = gets.chomp
-
-puts "choice of user name"
-user_name = gets.chomp
-
-
-
-
-create_user(db,name,email,user_name)
-userid = (db.execute("SELECT * FROM users Where name = '#{name}' ")).first['id']
-puts userid
-valid_input = false
-until valid_input == true 
-	specific_user(db,userid)
-	puts " Is this correct Y/N"
-	spec_ans = gets.chomp.upcase
-	if spec_ans =="Y"
+	if member == "new" 
 		valid_input = true
-		p "Thanks for your registration"
-		elsif spec_ans == "N"
-			puts "what is your id?"
-				idprovided = gets.chomp.to_i
-	 	 	p "Which will you like to change? name,email or username"
-	 	 	veri_ans = gets.chomp
-		 	if veri_ans == "name"
-		 	 		puts "What will you like to update it to"
-		 	 		name1 = gets.chomp.split(" ")
-						name1.map! do|i|
-						 i.capitalize
-						end
-					updatedname = name1.join(" ")
-					updatename(db,updatedname,idprovided)
-		 	 	elsif veri_ans == "email" 
-		 	 		puts "What email will you like to update to"
-		 	 		updatedemail = gets.chomp
-		 	 		updateemail(db,updatedemail,idprovided)
-		 	 	elsif veri_ans == "username"
-		 	 		puts "What will be your user name"
-		 	 		updateuser_name = gets.chomp
-		 	 		updateusername(db,updateuser_name,idprovided)
-		 	end
+		puts "We will like to gather your Info for Purchase"
+		puts "What is your full name?,First name Last name"
 
-		else puts "I do not understand you!!!"
+		name1 = gets.chomp.split(" ")
+
+		name1.map! do|i|
+		 i.capitalize
+		end
+
+		name = name1.join(" ")
+
+		puts "What is your email"
+
+		email = gets.chomp
+
+		puts "choice of user name"
+		user_name = gets.chomp
+
+
+
+
+		create_user(db,name,email,user_name)
+		userid = (db.execute("SELECT * FROM users Where name = '#{name}' ")).first['id']
+		# puts userid
+		new_member(db,userid)
+		
+		elsif member == "current"
+		valid_input = true
+		puts "What is your userid"
+		userid = gets.chomp.to_i
+		specific_user(db,userid)
+		puts "Thanks for being a valuable customer"
+		else puts "Just confirming are you a new or current member. TYPE new or current"
 	end
 end
-
-
-
-
 
 
 # Insert user Information Into Database
